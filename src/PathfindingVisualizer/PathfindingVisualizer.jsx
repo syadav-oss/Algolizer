d38173c62c3385f14586604e2823757493683f5d
Shashank11122001 <goyal.shashank2000@gmail.com>
2021-05-21 14:35:45 +0530

    Modified Navbar

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -90,12 +90,18 @@ export default class PathfindingVisualizer extends Component {
   render() {
     return (
       <div>
-        <Navbar bg="dark" expand="lg" variant="dark">
-          <Navbar.Brand href="#">Path Finding Visualizer</Navbar.Brand>
+        <Navbar bg="dark" expand="lg" variant="dark" className="logo">
+          <Navbar.Brand href="#" className="ml-5">
+            Path Finding Visualizer
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse className="nav" id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <NavDropdown title="Algorithm" id="basic-nav-dropdown">
+            <Nav className="mr-auto col-centered1">
+              <NavDropdown
+                title="Algorithm"
+                id="basic-nav-dropdown"
+                className="mr-3"
+              >
                 <NavDropdown.Item href="#action/3.1">
                   Dijkstra's Algorithm
                 </NavDropdown.Item>
@@ -110,7 +116,11 @@ export default class PathfindingVisualizer extends Component {
                   Breadth-First Search
                 </NavDropdown.Item>
               </NavDropdown>
-              <NavDropdown title="Mazes & Patterns" id="basic-nav-dropdown">
+              <NavDropdown
+                title="Mazes & Patterns"
+                id="basic-nav-dropdown"
+                className="mr-3"
+              >
                 <NavDropdown.Item href="#action/3.1">
                   Recursive Division
                 </NavDropdown.Item>
@@ -121,12 +131,26 @@ export default class PathfindingVisualizer extends Component {
                   Simple Stair Pattern
                 </NavDropdown.Item>
               </NavDropdown>
-              <Nav.Link href="#add">Add Bomb</Nav.Link>
-              <Button variant="outline-success">Visualise</Button>
-              <Nav.Link href="#board">Clear Board</Nav.Link>
-              <Nav.Link href="#wall">Clear Walls & Weights</Nav.Link>
-              <Nav.Link href="#path">Clear Path</Nav.Link>
-              <NavDropdown title="Speed" id="basic-nav-dropdown">
+              <Nav.Link href="#add" className="mr-3">
+                Add Bomb
+              </Nav.Link>
+              <Button variant="outline-success" className="mr-3">
+                Visualise
+              </Button>
+              <Nav.Link href="#board" className="mr-3">
+                Clear Board
+              </Nav.Link>
+              <Nav.Link href="#wall" className="mr-3">
+                Clear Walls & Weights
+              </Nav.Link>
+              <Nav.Link href="#path" className="mr-3">
+                Clear Path
+              </Nav.Link>
+              <NavDropdown
+                title="Speed"
+                id="basic-nav-dropdown"
+                className="mr-3"
+              >
                 <NavDropdown.Item href="#action/3.1">Slow</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Average</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.3">Fast</NavDropdown.Item>
@@ -136,7 +160,7 @@ export default class PathfindingVisualizer extends Component {
         </Navbar>
 
         <Navbar bg="light" expand="lg" variant="light">
-          <Navbar.Brand>Start Node</Navbar.Brand>
+          <Navbar.Brand className="col-centered">Start Node</Navbar.Brand>
           <Navbar.Brand>Target Node</Navbar.Brand>
           <Navbar.Brand>Bomb Node</Navbar.Brand>
           <Navbar.Brand>Weight Node</Navbar.Brand>
