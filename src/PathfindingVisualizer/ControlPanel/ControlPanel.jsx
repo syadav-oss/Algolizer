d2d982b5ab277b32e73e5629f724905d67a2fb33
Pulkit Changoiwala <changoiwalapulkit@gmail.com>
2021-05-22 00:35:54 +0530

    Control Panel Component

---
+++ src/PathfindingVisualizer/ControlPanel/ControlPanel.jsx
@@ -0,0 +1,105 @@
+import React, { Component } from "react";
+import "./ControlPanel.css";
+
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+  Container,
+} from "react-bootstrap";
+export default class ControlPanel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <Navbar bg="dark" expand="lg" variant="dark" className="logo">
+          <Navbar.Brand href="#" className="ml-5">
+            Path Finding Visualizer
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse className="nav" id="basic-navbar-nav">
+            <Nav className="mr-auto col-centered1">
+              <NavDropdown
+                title="Algorithm"
+                id="basic-nav-dropdown"
+                className="mr-3"
+              >
+                <NavDropdown.Item href="#action/3.1">
+                  Dijkstra's Algorithm
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  A* Algorithm
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.3">
+                  Depth-First Search
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.4">
+                  Breadth-First Search
+                </NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown
+                title="Mazes & Patterns"
+                id="basic-nav-dropdown"
+                className="mr-3"
+              >
+                <NavDropdown.Item href="#action/3.1">
+                  Recursive Division
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Basic Random Maze
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Simple Stair Pattern
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="#add" className="mr-3">
+                Add Bomb
+              </Nav.Link>
+              <Button variant="outline-success" className="mr-3">
+                Visualise
+              </Button>
+              <Nav.Link href="#board" className="mr-3">
+                Clear Board
+              </Nav.Link>
+              <Nav.Link href="#wall" className="mr-3">
+                Clear Walls & Weights
+              </Nav.Link>
+              <Nav.Link href="#path" className="mr-3">
+                Clear Path
+              </Nav.Link>
+              <NavDropdown
+                title="Speed"
+                id="basic-nav-dropdown"
+                className="mr-3"
+              >
+                <NavDropdown.Item href="#action/3.1">Slow</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Average</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Fast</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+
+        <Navbar bg="light" expand="lg" variant="light">
+          <Navbar.Brand className="col-centered">Start Node</Navbar.Brand>
+          <Navbar.Brand>Target Node</Navbar.Brand>
+          <Navbar.Brand>Bomb Node</Navbar.Brand>
+          <Navbar.Brand>Weight Node</Navbar.Brand>
+          <Navbar.Brand>Unvisited Node</Navbar.Brand>
+          <Navbar.Brand>Visited Node</Navbar.Brand>
+          <Navbar.Brand>Shortest Path Node</Navbar.Brand>
+          <Navbar.Brand>Wall Node</Navbar.Brand>
+        </Navbar>
+
+        <br />
+      </div>
+    );
+  }
+}
--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -1,15 +1,7 @@
 import React, { Component } from "react";
 import Node from "./Node/Node";
+import ControlPanel from "./ControlPanel/ControlPanel";
 import "./PathfindingVisualizer.css";
-import {
-  Navbar,
-  Nav,
-  NavDropdown,
-  Form,
-  FormControl,
-  Button,
-  Container,
-} from "react-bootstrap";
 
 let StartNodeRow = 5;
 let StartNodeCol = 5;
@@ -136,88 +128,7 @@ export default class PathfindingVisualizer extends Component {
   render() {
     return (
       <div>
-        <Navbar bg="dark" expand="lg" variant="dark" className="logo">
-          <Navbar.Brand href="#" className="ml-5">
-            Path Finding Visualizer
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse className="nav" id="basic-navbar-nav">
-            <Nav className="mr-auto col-centered1">
-              <NavDropdown
-                title="Algorithm"
-                id="basic-nav-dropdown"
-                className="mr-3"
-              >
-                <NavDropdown.Item href="#action/3.1">
-                  Dijkstra's Algorithm
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  A* Algorithm
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.3">
-                  Depth-First Search
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.4">
-                  Breadth-First Search
-                </NavDropdown.Item>
-              </NavDropdown>
-              <NavDropdown
-                title="Mazes & Patterns"
-                id="basic-nav-dropdown"
-                className="mr-3"
-              >
-                <NavDropdown.Item href="#action/3.1">
-                  Recursive Division
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Basic Random Maze
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Simple Stair Pattern
-                </NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link href="#add" className="mr-3">
-                Add Bomb
-              </Nav.Link>
-              <Button variant="outline-success" className="mr-3">
-                Visualise
-              </Button>
-              <Nav.Link href="#board" className="mr-3">
-                Clear Board
-              </Nav.Link>
-              <Nav.Link href="#wall" className="mr-3">
-                Clear Walls & Weights
-              </Nav.Link>
-              <Nav.Link href="#path" className="mr-3">
-                Clear Path
-              </Nav.Link>
-              <NavDropdown
-                title="Speed"
-                id="basic-nav-dropdown"
-                className="mr-3"
-              >
-                <NavDropdown.Item href="#action/3.1">Slow</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Average</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Fast</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-
-        <Navbar bg="light" expand="lg" variant="light">
-          <Navbar.Brand className="col-centered">Start Node</Navbar.Brand>
-          <Navbar.Brand>Target Node</Navbar.Brand>
-          <Navbar.Brand>Bomb Node</Navbar.Brand>
-          <Navbar.Brand>Weight Node</Navbar.Brand>
-          <Navbar.Brand>Unvisited Node</Navbar.Brand>
-          <Navbar.Brand>Visited Node</Navbar.Brand>
-          <Navbar.Brand>Shortest Path Node</Navbar.Brand>
-          <Navbar.Brand>Wall Node</Navbar.Brand>
-        </Navbar>
-
-        <br />
-
+        <ControlPanel></ControlPanel>
         <div className="grid">
           {this.state.grid.map((row, rowId) => {
             return (
