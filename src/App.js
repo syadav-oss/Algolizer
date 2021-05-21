1d75ea6fec310de348b73942821458064b7267e7
Shashank11122001 <goyal.shashank2000@gmail.com>
2021-05-21 12:24:54 +0530

    Added NavBar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import './App.css';
-import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import PathfindingVisualizer from "./PathfindingVisualizer/PathfindingVisualizer";
 
 function App() {
   return (
--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
 import Node from "./Node/Node";
 import "./PathfindingVisualizer.css";
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+  Container,
+} from "react-bootstrap";
 
 const GridRowSize = 25;
 const GridColSize = 50;
@@ -80,34 +89,96 @@ export default class PathfindingVisualizer extends Component {
 
   render() {
     return (
-      <div className="grid">
-        {this.state.grid.map((row, rowId) => {
-          return (
-            <div key={rowId}>
-              {row.map((node, nodeId) => {
-                const { col, row, isFinish, isStart, isWall } = node;
-                return (
-                  <Node
-                    key={nodeId}
-                    col={col}
-                    row={row}
-                    isFinish={isFinish}
-                    isStart={isStart}
-                    isWall={isWall}
-                    onMouseDown_={(row, col) => this.handleMouseDown(row, col)}
-                    onMouseUp_={(row, col) => this.handleMouseUp(row, col)}
-                    onMouseEnter_={(row, col) =>
-                      this.handleMouseEnter(row, col)
-                    }
-                    onMouseLeave_={(row, col) =>
-                      this.handleMouseLeave(row, col)
-                    }
-                  ></Node>
-                );
-              })}
-            </div>
-          );
-        })}
+      <div>
+        <Navbar bg="dark" expand="lg" variant="dark">
+          <Navbar.Brand href="#">Path Finding Visualizer</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse className="nav" id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <NavDropdown title="Algorithm" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">
+                  Dijkstra's Algorithm
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  A* Algorithm
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.3">
+                  Depth-First Search
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.4">
+                  Breadth-First Search
+                </NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title="Mazes & Patterns" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">
+                  Recursive Division
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Basic Random Maze
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Simple Stair Pattern
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="#add">Add Bomb</Nav.Link>
+              <Button variant="outline-success">Visualise</Button>
+              <Nav.Link href="#board">Clear Board</Nav.Link>
+              <Nav.Link href="#wall">Clear Walls & Weights</Nav.Link>
+              <Nav.Link href="#path">Clear Path</Nav.Link>
+              <NavDropdown title="Speed" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Slow</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Average</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Fast</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+
+        <Navbar bg="light" expand="lg" variant="light">
+          <Navbar.Brand>Start Node</Navbar.Brand>
+          <Navbar.Brand>Target Node</Navbar.Brand>
+          <Navbar.Brand>Bomb Node</Navbar.Brand>
+          <Navbar.Brand>Weight Node</Navbar.Brand>
+          <Navbar.Brand>Unvisited Node</Navbar.Brand>
+          <Navbar.Brand>Visited Node</Navbar.Brand>
+          <Navbar.Brand>Shortest Path Node</Navbar.Brand>
+          <Navbar.Brand>Wall Node</Navbar.Brand>
+        </Navbar>
+
+        <br />
+
+        <div className="grid">
+          {this.state.grid.map((row, rowId) => {
+            return (
+              <div key={rowId}>
+                {row.map((node, nodeId) => {
+                  const { col, row, isFinish, isStart, isWall } = node;
+                  return (
+                    <Node
+                      key={nodeId}
+                      col={col}
+                      row={row}
+                      isFinish={isFinish}
+                      isStart={isStart}
+                      isWall={isWall}
+                      onMouseDown_={(row, col) =>
+                        this.handleMouseDown(row, col)
+                      }
+                      onMouseUp_={(row, col) => this.handleMouseUp(row, col)}
+                      onMouseEnter_={(row, col) =>
+                        this.handleMouseEnter(row, col)
+                      }
+                      onMouseLeave_={(row, col) =>
+                        this.handleMouseLeave(row, col)
+                      }
+                    ></Node>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
